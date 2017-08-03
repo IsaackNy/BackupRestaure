805d9e9bf86ffd23a535318df25771185d82dd21
IsaackNy <inyobe@yahoo.ca>
2017-08-03 14:49:08 -0400

    Add files via upload

---
+++ RestoreWithRecovery.sql
@@ -0,0 +1,3 @@
+--The following example recovers the AdventureWorks2012 sample database in a restore operation without restoring data.
+-- Restore database using WITH RECOVERY.
+RESTORE DATABASE AdventureWorks2012    WITH RECOVERY
